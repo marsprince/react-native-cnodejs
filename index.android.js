68f768fb9a88f2723ff293148c3a1e72dc05ab08
marsprince <bit_liujia@163.com>
2015-10-10 17:15:48 +0800

    Initial commit

---
+++ index.android.js
@@ -0,0 +1,35 @@
+var React = require('react-native')
+var App = require('./app/containers/App')
+
+//var user = require('./app/mocks/user')
+//var userInfo = require('./app/mocks/userInfo')
+//var UserService = require('./app/services/UserService')
+//Object.assign(user, userInfo)
+
+
+// on run
+require('./app/configs/onRun')
+
+
+
+var {
+    Component,
+    AppRegistry
+    } = React
+
+
+class Noder extends Component {
+
+  componentDidMount() {
+    //UserService.storage.saveUser(user)
+    //UserService.storage.clearUser()
+  }
+
+  render() {
+    return (
+        <App></App>
+    )
+  }
+}
+
+AppRegistry.registerComponent('cnodejs', () => Noder)
