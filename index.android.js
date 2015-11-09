4ff7e90d20628afa2fcb1d96be8bbc386070e840
marsprince <bit_liujia@163.com>
2015-11-09 08:41:19 +0800

    update

--- index.android.js
+++ index.android.js
@@ -3,7 +3,10 @@ var React = require('react-native')
 var {
     Component,
     AppRegistry,
-    View
+    View,
+    Image,
+    Text,
+    StyleSheet
     } = React
 
 var Example=require('./example/containers/app');
@@ -23,5 +26,73 @@ class Noder extends Component {
     )
   }
 }
+class Test extends Component {
 
+    componentDidMount() {
+        //UserService.storage.saveUser(user)
+        //UserService.storage.clearUser()
+    }
+
+    render() {
+        var fullImage = {uri: 'launcher_icon'};
+        return (
+            <View >
+                <Image
+                    style={[styles.base, {borderRadius: 5}]}
+                    //source={require('./logo_og.png')}
+                    source={fullImage}
+                    />
+            </View>
+
+        )
+    }
+}
+var styles = StyleSheet.create({
+    base: {
+        width: 500,
+        height: 500,
+    },
+    progress: {
+        flex: 1,
+        alignItems: 'center',
+        flexDirection: 'row',
+        width: 100
+    },
+    leftMargin: {
+        marginLeft: 10,
+    },
+    background: {
+        backgroundColor: '#222222'
+    },
+    sectionText: {
+        marginVertical: 6,
+    },
+    nestedText: {
+        marginLeft: 12,
+        marginTop: 20,
+        backgroundColor: 'transparent',
+        color: 'white'
+    },
+    resizeMode: {
+        width: 90,
+        height: 60,
+        borderWidth: 0.5,
+        borderColor: 'black'
+    },
+    resizeModeText: {
+        fontSize: 11,
+        marginBottom: 3,
+    },
+    icon: {
+        width: 15,
+        height: 15,
+    },
+    horizontal: {
+        flexDirection: 'row',
+    },
+    gif: {
+        flex: 1,
+        height: 200,
+    },
+});
 AppRegistry.registerComponent('cnodejs', () => Noder)
